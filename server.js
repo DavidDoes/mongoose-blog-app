7c8ad17655992c791999b29452cd8266b82051fd
David Clark <dlc.clark@gmail.com>
2018-07-29 16:40:48 -0700

    remove unnecessary line

--- server.js
+++ server.js
@@ -66,7 +66,6 @@ app.post('/posts', (req, res) => {
 })
 
 app.put('/posts/:id', (req, res) => {
-    const requiredFields = ["id"]
     if(!(req.params.id && req.body.id && req.params.id === req.body.id)) {
         const message = 
             `Request path id (${req.params.id}) and request body id ` +
